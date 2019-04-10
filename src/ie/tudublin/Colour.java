16b2f5f0b755cadc00391a46bb87dc01e2afd343
Jonathan-Unsworth <jonathanunsworth117@gmail.com>
2019-04-10 16:24:28 +0100

    Colour class implementation

--- src/ie/tudublin/Colour.java
+++ src/ie/tudublin/Colour.java
@@ -1,6 +1,76 @@
 package ie.tudublin;
 
 public class Colour {
+    public int r, g, b;
+    private String colour;
 
+    public Colour(int r, int g, int b) {
+        setR(r);
+        setG(g);
+        setB(b);
+    }
+
+    public String toString() {
+        return Integer.toString(r) + " " + Integer.toString(g) + " " + Integer.toString(b);
+    }
+
+
+
+
+    /**
+     * @return the r
+     */
+    public int getR() {
+        return r;
+    }
+
+    /**
+     * @param r the r to set
+     */
+    public void setR(int r) {
+        this.r = r;
+    }
+
+    /**
+     * @return the g
+     */
+    public int getG() {
+        return g;
+    }
+
+    /**
+     * @param g the g to set
+     */
+    public void setG(int g) {
+        this.g = g;
+    }
+
+    /**
+     * @return the b
+     */
+    public int getB() {
+        return b;
+    }
+
+    /**
+     * @param b the b to set
+     */
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    /**
+     * @return the colour
+     */
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
     
 }
